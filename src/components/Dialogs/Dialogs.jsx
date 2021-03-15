5ae446649d0ba37214a5662e78536f7677b24ad8
DM-MRTNVCH <dmvshn@gmail.com>
2021-03-16 02:41:00 +0300

    Dialogs.jsx Dialogs.module.css

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,30 @@ import s from './Dialogs.module.css'
 
 function Dialogs(){
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={`${s.dialog} ${s.active}`}>
+                    first name
+                </div>
+                <div className={s.dialog}>
+                    second name
+                </div>
+                <div className={s.dialog}>
+                    third name
+                </div>
+                <div className={s.dialog}>
+                    fourth name
+                </div>
+                <div className={s.dialog}>
+                    fifth name
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>raz msg</div>
+                <div className={s.message}>dva msg</div>
+                <div className={s.message}>tri msg</div>
+
+            </div>
         </div>
     )
 }
