120c35f4b56a723a0a3ca6ea9d5a15b8ecec010d
DM-MRTNVCH <dmvshn@gmail.com>
2021-05-31 19:02:09 +0300

    DAL, axios.create

---
+++ src/api/api.js
@@ -0,0 +1,21 @@
+import * as axios from 'axios';
+
+const instance = axios.create({
+    withCredentials: true,
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    headers: {
+        'API-KEY': 'dedd7c3f-c8fc-47f5-80ed-986730aa4e9e'
+    }
+})
+
+
+export const usersAPI = {
+    getUsers(currentPage = 1, pageSize = 10){
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`)
+            .then(response => response.data)
+    }
+}
+
+// export const getUsers2 = (currentPage = 1, pageSize = 10) => {
+//     return instance.get(`users?page=${currentPage}&count=${pageSize}`).then(response => response.data)
+// }
